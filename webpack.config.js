1bbb001dbfc5683a6633f3dcad6e752804077b07
Simon Hudson <simon.hudson@experian.com>
2019-11-07 16:39:20 +0000

    add cross-env

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,6 @@ const envs = {
 };
 
 /* eslint-disable global-require,import/no-dynamic-require */
-console.log('--------------------');
-console.log(process.env.NODE_ENV);
-console.log('--------------------');
 const env = envs[process.env.NODE_ENV || 'development'];
 const envConfig = require(`./config/webpack/webpack.${env}.babel`);
 module.exports = webpackMerge(common, envConfig);
