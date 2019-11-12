eab603184578ebce67014f8ba56d8bd261c49dfd
Simon Hudson <simon.hudson@experian.com>
2019-11-12 08:29:12 +0000

    lint

--- src/App.js
+++ src/App.js
@@ -4,16 +4,12 @@ import React, { Component } from 'react';
 import './assets/css/styles.scss';
 import { getClub, getFixtures, getResults } from './functions';
 import { transformSquad, transformResults } from './transform';
-import ClubPicker from './components/club-picker';
 import Fixtures from './components/views/fixtures';
 import Header from './components/header';
 import Home from './components/views/home';
 import MainNavigation from './components/navigation/main-navigation';
 import Results from './components/views/results';
 import Squad from './components/views/squad';
-import { deleteLocalStorage, getLocalStorage, setLocalStorage } from '~/utilities/storage/local';
-
-const CLUB_ID_STORAGE_KEY = 'clubId';
 
 class App extends Component {
 
@@ -56,20 +52,7 @@ class App extends Component {
     }
 
     componentDidMount = () => {
-        if (this.state.clubId) this.getData();
-    }
-    
-    componentDidUpdate = () => {
-        if (this.state.clubId) this.getData();
-    }
-    
-    setClub = e => {
-        e.preventDefault ? e.preventDefault() : (e.returnValue = false);
-        this.setState({ clubId: parseInt(e.target.value, 10) });
-    }
-    
-    clearClub = () => {
-        this.setState({ clubId: null });
+        this.getData();
     }
     
     onNavigationClick = e => {
@@ -84,19 +67,14 @@ class App extends Component {
         
         return (
             <div className="wrap">
-                {this.state.clubId && (
-                    <>
-                        <Header clubName={state.clubName} src={state.crestUrl} onClearClubClick={this.clearClub}/>
-                        <main>
-                            {state.currentView === 'home' && <Home {...state} />}
-                            {state.currentView === 'results' && <Results {...state} />}
-                            {state.currentView === 'fixtures' && <Fixtures {...state} />}
-                            {state.currentView === 'squad' && <Squad {...state} />}
-                        </main>
-                        <MainNavigation onNavigationClick={this.onNavigationClick} />
-                    </>
-                )}
-                {!this.state.clubId && (<ClubPicker onChange={this.setClub}/>)}
+                <Header clubName={state.clubName} src={state.crestUrl} onClearClubClick={this.clearClub}/>
+                <main>
+                    {state.currentView === 'home' && <Home {...state} />}
+                    {state.currentView === 'results' && <Results {...state} />}
+                    {state.currentView === 'fixtures' && <Fixtures {...state} />}
+                    {state.currentView === 'squad' && <Squad {...state} />}
+                </main>
+                <MainNavigation onNavigationClick={this.onNavigationClick} />
             </div>
         );
     }
