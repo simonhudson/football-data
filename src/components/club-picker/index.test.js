336296bf553926313ab0b3524921f7add867d457
Hudson <Simon.Hudson@experian.com>
2019-11-13 13:49:17 +0000

    format

--- src/components/club-picker/index.test.js
+++ src/components/club-picker/index.test.js
@@ -5,16 +5,16 @@ import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import ClubPicker from './index';
 
-const testOptions = require('~/test-data/club-picker-options'); 
+const testOptions = require('~/test-data/club-picker-options');
 
 describe('ClubPicker', () => {
-    
+
     let objectUnderTest;
-	const selector = `[data-test="club-picker"]`;
+    const selector = `[data-test="club-picker"]`;
     const label = `[data-test="club-picker__label"]`;
     const select = `[data-test="club-picker__select"]`;
     const option = `[data-test="club-picker__option"]`;
-    
+
     it('should return null when no options in state', () => {
         initialise();
         expect(objectUnderTest.html()).to.be.null;
@@ -23,7 +23,7 @@ describe('ClubPicker', () => {
         expect(objectUnderTest.find(label).length).to.equal(0);
         expect(objectUnderTest.find(select).length).to.equal(0);
     });
-    
+
     it('should render options list from state', () => {
         initialise();
         expect(objectUnderTest.find(selector).length).to.equal(0);
@@ -36,7 +36,7 @@ describe('ClubPicker', () => {
         expect(objectUnderTest.find(select).length).to.equal(1);
         expect(objectUnderTest.find(option).length).to.equal(4);
     });
-    
+
     it('should render correct values and text for options', () => {
         initialise();
         expect(objectUnderTest.find(selector).length).to.equal(0);
@@ -53,5 +53,5 @@ describe('ClubPicker', () => {
     });
 
     const initialise = () => objectUnderTest = shallow(<ClubPicker />);
-    
+
 });
