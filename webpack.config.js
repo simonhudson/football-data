9754cc4cfec25bd063dea49f54ba4ebc7ab87154
Simon Hudson <simon.hudson@experian.com>
2019-12-02 08:52:45 +0000

    debug env

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,6 @@ const envs = {
 
 /* eslint-disable global-require,import/no-dynamic-require */
 const env = envs[process.env.NODE_ENV || 'development'];
+console.log(`>>> : ${env}`);
 const envConfig = require(`./config/webpack/webpack.${env}.babel`);
 module.exports = webpackMerge(common, envConfig);
